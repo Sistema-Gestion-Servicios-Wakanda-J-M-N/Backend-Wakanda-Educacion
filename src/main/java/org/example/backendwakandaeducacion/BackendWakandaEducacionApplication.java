d30d89bad154925dcfef40e6ecebdf0c972bf247
marcos <marcgabe@myuax.com>
2024-11-26 13:21:07 +0100

    Arquitectura V 1.0
    Inicial Commit

---
+++ src/main/java/org/example/backendwakandaeducacion/BackendWakandaEducacionApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendwakandaeducacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWakandaEducacionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendWakandaEducacionApplication.class, args);
+    }
+
+}
